c385f235a6060b923e397a2f57a1226f05af0f8d
UsuarioM <espacioguriats@gmail.com>
2023-07-17 14:07:42 +0200

    Portfolio

---
+++ data/projects.js
@@ -0,0 +1,47 @@
+export const projects = [
+    {
+    title: "Primer proyecto",
+    description: "Esta es la descripción de mi primer proyecto",
+    image:
+    "https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto1",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Segundo proyecto",
+    description: "Esta es la descripción de mi segundo proyecto",
+    image:
+    "https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto2",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Tercer proyecto",
+    description: "Esta es la descripción de mi tercer proyecto",
+    image:
+    "https://images.unsplash.com/photo-1509343256512-d77a5cb3791b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto3",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Cuarto proyecto",
+    description: "Esta es la descripción de mi cuarto proyecto",
+    image:
+    "https://images.unsplash.com/photo-1541462608143-67571c6738dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto4",
+    link: "www.proyecto1.com",
+    },
+    {
+    title: "Quinto proyecto",
+    description: "Esta es la descripción de mi quinto proyecto",
+    image:
+    "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    tech: ["React", "HTML5", "CSS3", "JavaScript"],
+    github: "http://github.com/myuser/proyecto5",
+    link: "www.proyecto1.com",
+    },
+    ];
